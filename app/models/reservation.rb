49330989ff2085dc7fbcf056e73f2b907b9ad0ca
valdife <salemon876@gmail.com>
2023-01-09 21:39:40 +0000

    Names refactor, enable validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,9 +4,9 @@ class Reservation < ApplicationRecord
   validates :last_name, presence: true, length: {minimum: 2, maximum: 25}
   validates :phone, presence: true, length: {is:9}, format: {with: /\A[+-]?\d+\z/, message: "only allows numbers"} 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validate :oldEvent
+  validate :old_event
 
-  def oldEvent
+  def old_event
     if event.start_time < Date.today
       errors.add(:event,"się odbywa lub odbyło")
     end
